67cb75fd59bdbbb1620596e43cde7efd73b6d014
quanpower <quanpower@gmail.com>
2018-02-07 20:01:02 +0800

    init

---
+++ extensions.py
@@ -0,0 +1,17 @@
+# Setup the configuration for login manager.
+#     1. Set the login page.
+#     2. Set the more stronger auth-protection.
+#     3. Show the information when you are logging.
+#     4. Set the Login Messages type as `information`.
+login_manager.login_view = "main.login"
+login_manager.session_protection = "strong"
+login_manager.login_message = "Please login to access this page."
+login_manager.login_message_category = "info"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Load the user's info."""
+
+    from models import User
+    return User.query.filter_by(id=user_id).first()
